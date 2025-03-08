dacc3e28f73e2abf44534c4ea5577cbe81e5230d
Adrian Tam <azianblank2@gmail.com>
2025-03-07 23:52:46 -1000

    Progress on the self-hosted server
    Fixed Dependency Errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 
 const inter = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,13 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn("relative h-full font-sans antialiased", inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
+
+          <Providers>
+
           <Navbar />
           <div className='flex-grow flex-1'>{children}</div>
+          
+          </Providers>
           </main>
       </body>
     </html>
