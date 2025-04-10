e6f752f180e9cd6dbcc97964a80435982fd48c32
Adrian Tam <azianblank2@gmail.com>
2025-04-10 12:36:11 -1000

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+        pathname: '**',
+        port: '3000',
+        protocol: 'http',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
