386786b604f88530d047ce88cb3d4db432c9d4ce
Adrian Tam <azianblank2@gmail.com>
2025-04-10 20:24:31 -1000

    2

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,5 +1,9 @@
+import ImageSlider from "@/components/ImageSlider"
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import { PRODUCT_CATEGORIES } from "@/config"
 import { getPayloadClient } from "@/get-payload"
+import { formatPrice } from "@/lib/utils"
+import { Check } from "lucide-react"
 import Link from "next/link"
 import { notFound } from "next/navigation"
 
@@ -40,7 +44,19 @@ const Page = async ({params}: PageProps) => {
     })
 
     const [product] = products
-    if(!product) return notFound()
+    if(!product) {
+        console.log("No product")
+        return notFound()
+    } 
+
+    const label = PRODUCT_CATEGORIES.find(
+        ({ value }) => value === product.category
+    )?.label
+
+    const validUrls = product.images
+        .map(({image}) => 
+        typeof image === "string" ? image : image.url
+    ).filter(Boolean) as string[]
 
     return <MaxWidthWrapper className="bg-white">
         <div className="bg-white">
@@ -70,9 +86,34 @@ const Page = async ({params}: PageProps) => {
                     </ol>
 
                     <div className="mt-4">
-                        <h1>{product.id}</h1>
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{product.name}</h1>
                     </div>
 
+                    <section className="mt-4">
+                        <div className="flex items-center">
+                            <p className="font-medium text-gray-900">{formatPrice(product.price)}</p>
+                            <div className="ml-4 border-l text-muted-foreground border-gray-300 pl-4">
+                                {label}
+                            </div>
+                        </div>
+
+                        <div className="mt-4 space-y-6">
+                            <p className="text-base text-muted-foreground">
+                                {product.description}
+                            </p>
+                        </div>
+
+                        <div className="mt-6 flex items-center">
+                            <Check aria-hidden='true' className="h-5 w-5 flex-shrink-0 text-green-500" />
+                            <p className="ml-2 text-sm text-muted-foreground">Eligible for instant delivery</p>
+                        </div>
+                    </section>
+                </div>
+                {/* Product Images */}
+                <div className="mt-10 lg:col-start-2 lg:row-start-2 lg:mt-2 lg:self-center">
+                    <div className="aspect-square rounded-lg">
+                        <ImageSlider urls={validUrls} />
+                    </div>
                 </div>
             </div>
         </div>
